7605bb6db8769c6d467303303d7202aea4c90474
Logan Coates <coates.logan1125@gmail.com>
2020-11-20 19:55:25 -0700

    finished the js

--- app.js
+++ app.js
@@ -115,27 +115,73 @@ function teamMember() {
                         validate: confirmName
                     }
                 ])
-                
-            }
-        }
-    }
 
-    
+                .then(function (answers) {
+                    let engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+                            engagementTeam.push(engineer);
+                            chooseMemberNext();
+                })
+                .catch(function(err) {
+                    console.log(err);
+                  });
+
+        } else if (teamChoice.team === 'Intern') {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What is your intern's name?",
+                    name: "name",
+                    validate: confirmName
+                },
+                {
+                    type: "input",
+                    message: "What is your intern's id?",
+                    name: "id",
+                    validate: confirmNumber
+                },
+                {
+                    type: "input",
+                    message: "What is your intern's email?",
+                    name: "email",
+                    validate: validateEmail 
+                },
+                {
+                    type: "input",
+                    message: "What is your intern's school?",
+                    name: "school",
+                    validate: confirmName
+                }
+            ])
+                .then(function (answers) {
+                    let intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+                    engagementTeam.push(intern);
+                    chooseMemberNext();
+                })
+                .catch(function(err) {
+                    console.log(err);
+                  });
+
+        } else {generateFile()}
+
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+// Loop back to original question for engineer or intern and begin again until user calls "I don't want to add
+//anymore team members", at which point the loop stops
+
 
 
 }
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+teamMember();
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+//Call the function
+
+
+
+
+function generateFile() {
+fs.writeFileSync(outputPath, render(engagementTeam),"utf-8")
+}
